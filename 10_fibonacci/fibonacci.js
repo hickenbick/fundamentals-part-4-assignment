1fe035815c5369e0b0f0d6b2d7d17aace840d8e1
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-31 11:24:16 +0000

    Exercise 10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let a = 0;
+  b = 1;
+  c = n;
+ 
+  if ( n > -1) {
+   for(let i = 2; i <= n; i++) {
+     c = a + b;
+     a = b; 
+     b = c;
+    }
+    return +c;
+   } else {
+     return "OOPS"
+   }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+ 
+
+// console.log(fibonacci("1"))
