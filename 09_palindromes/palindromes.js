e8269c4795992b038fd291e55313b514bc260334
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-31 10:09:10 +0000

    Palindrome exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function(str) {
+  let re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, "");
+  var reverseStr = lowRegStr.split("").reverse().join("")
+  return reverseStr === lowRegStr
+}
 
-};
+// console.log(palindromes("A man, a plan, a canal. Panama"));
 
 // Do not edit below this line
 module.exports = palindromes;
