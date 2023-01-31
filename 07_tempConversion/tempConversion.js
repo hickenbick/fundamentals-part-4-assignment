b0b0df53d229d814b086ce1262931cc3a36c22b0
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-31 10:46:06 +0000

    Exercise 07_tempconversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = ((f - 32) / 1.800)
+  return round(c, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9/5) +32;
+  return round(f, 1) 
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
