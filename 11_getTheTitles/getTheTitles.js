75181fbf330c3e349b51e39aafa730d761c1dcf0
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-02-03 10:07:42 +0000

    Exercise 11_getTheTitles completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,28 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title:"Book",
+    author: "Author"
+  },
+  {
+    title:"Book1",
+    author: "Author1"
+  },
+  {
+    title:"Book2",
+    author: "Author2"
+  },
+  {
+    title:"Book3",
+    author: "Author3"
+  }
+]
 
+let getTheTitles = function(arr){
+  let result = []
+  arr.map(function(obj) {
+    result.push(obj.title)
+  })
+  return result
 };
 
 // Do not edit below this line
